252db4acf57dd0e870318d07be05d6c116965753
Nathan Rothenberger <nroth10@gmail.com>
2020-05-27 10:19:55 -0700

    cleanup theme exports

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-const theme = {
+export const themeDark = {
   colors: {
     primary: "#0a8697",
     text: "white",
@@ -8,4 +8,14 @@ const theme = {
   },
 };
 
-export default theme;
+export const themeLight = {
+  colors: {
+    primary: "#ffdc00",
+    text: "#313131",
+    bg: "#dddddd",
+    asset: "#0a8697",
+  },
+};
+
+// export default themeDark as theme;
+export default themeDark;
