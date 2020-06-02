609d0a60b1c9871ce4a80afbf36303a3562a18cd
Nathan Rothenberger <nroth10@gmail.com>
2020-06-02 16:41:00 -0700

    add new review data. cleanup. new styles.

--- src/App2.js
+++ src/App2.js
@@ -6,6 +6,8 @@ import Card, { defaultCardState } from "./components/Card";
 import Token from "./components/Token";
 import { themeDark, themeLight } from "./theme";
 import Review, { SampleReview } from "./components/Review";
+import dan from "./assets/dan.jpeg";
+import reviews, { reviewDan } from "./data";
 
 const StyledAppHero = styled.main`
   background-color: ${({ theme }) => theme.colors.appBg};
@@ -20,19 +22,23 @@ const StyledAppHero = styled.main`
 `;
 
 const StyledReviewColumn = styled.div`
-  ${"" /* width: 80%; */}
   margin: 0 auto;
-  background: purple;
-  padding: 2em;
+  padding: 1em;
+  /**debug */
+  ${"" /* background: purple; */}
 `;
 
 const StyledHeader = styled.div`
   background-color: ${({ theme }) => theme.colors.appBg};
-  color: black;
-  padding: 1.5em 0;
   text-align: center;
+
   h1 {
-    font-size: 3em;
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 1.5em;
+    line-height: 1.3333333333;
+    padding: 0.83333333333em 0;
+    text-transform: uppercase;
+    letter-spacing: -0.5px;
   }
 
   @media screen and (min-width: 600px) {
@@ -73,9 +79,20 @@ function App() {
         </StyledHeader>
         <StyledAppHero className="App-header">
           <StyledReviewColumn>
-            <Review link={link} name={name} title={title} avatar={avatar}>
-              {SampleReview.reviewText}
-            </Review>
+            {reviews.map((review, i) => {
+              const { link, name, title, avatar, reviewText } = review;
+              return (
+                <Review
+                  link={link}
+                  name={name}
+                  title={title}
+                  avatar={avatar}
+                  reverse={i % 2}
+                >
+                  {reviewText}
+                </Review>
+              );
+            })}
           </StyledReviewColumn>
         </StyledAppHero>
       </div>
