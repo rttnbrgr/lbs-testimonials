1acbb919b7d27534af7593f6577c79325c643e07
Nathan Rothenberger <nroth10@gmail.com>
2020-06-02 16:40:49 -0700

    review styles

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,88 +1,147 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { css } from "@emotion/core";
+
 import { StyledHr } from "./misc";
 
-const StyledReview = styled.article`
-  ${"" /* width: 400px; */}
-  height: 300px;
-  background: black;
-  position: relative;
-  font-size: 1em;
-  padding: 2rem;
-  padding-bottom: 40px; // compensate for posiiton meta
-  ${"" /* margin-bottom: 4rem; */}
-
+const StyledReviewGroup = styled.article`
   & + & {
-    margin-top: 4rem;
+    margin-top: 1.5rem;
+    margin-top: 2.5rem;
+  }
+`;
+
+const StyledReview = styled.article`
+  background: rgba(0, 200, 0, 0.3);
+  position: relative;
+
+  margin-left: 1rem;
+  margin: 0 0 32px 16px;
+  p {
+    font-family: "Helvetica", sans-serif;
+    font-size: 1em;
+    line-height: 1.25;
   }
 
+  .box {
+    background: ${({ theme }) => theme.colors.text};
+    position: relative;
+    padding: 1em;
 
-
-  .avatar {
-    width: 64px;
-    height: 64px;
-    ${"" /* border-radius: 50%; */}
-    ${"" /* position: absolute; */}
-    ${"" /* top: 0;
-    left: 0;
-    transform: translate3d(-50%, -50%, 0); */}
-  }
-
-  .meta {
-    padding: 0.25em 0.5rem;
-    background: white;
-    border: 2px solid black;
-    color: black;
-    position: absolute;
-    top: 100%;
-    right: 0;
-    width: 50%;
-    width: content-width;
-    display: flex;
-    transform: translate3d(32px, -50%, 0);
-
-    h2 {
-      font-size: 1em;
-      line-height: 1.4;
-      font-family: "Helvetica", sans-serif;
-      font-weight: lighter;
-    }
-    h3 {
-      font-size: 0.875rem;
-      line-height: 1.4;
-      font-family: "Helvetica", sans-serif;
-      font-weight: bold;
+    &::after {
+      content: "";
+      position: absolute;
+      display: inline-block;
+      left: 0;
+      top: 100%;
+      border-top: 24px solid ${({ theme }) => theme.colors.text};
+      border-right: 24px solid transparent;
     }
   }
 
   a {
-    display: block;
-    padding: 1em 1em 1em 0;
-    font-size: 0.875rem;
-    text-transform: uppercase;
+    margin-top: 8px;
+    display: inline-block;
+    padding: 0.25em 0.5em 0.25em 0;
+    font-family: "Helvetica", sans-serif;
     font-weight: bold;
+    font-size: 0.75em;
+    line-height: 1.33333333;
     text-decoration: none;
-    color: inherit;
-  }
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.bg};
 
-  &.flip {
-    .avatar {
-      top: 0;
-      left: initial;
-      right: 0;
-      transform: translate3d(50%, -50%, 0);
+    &:hover {
+      color: ${({ theme }) => theme.colors.asset};
     }
   }
+
+  .triangle-row {
+    position: relative;
+    height: 24px;
+  }
+  .triangle {
+    position: absolute;
+    display: inline-block;
+    left: 0;
+    top: 0;
+    border-top: 24px solid ${({ theme }) => theme.colors.text};
+    border-right: 24px solid transparent;
+    display: none;
+  }
+
+  /* Reverse */
+  ${({ reverse, theme }) =>
+    reverse &&
+    css`
+      margin-right: 1em;
+      margin-left: 0;
+
+      .box {
+        &::after {
+          content: "";
+          position: absolute;
+          display: inline-block;
+          left: initial;
+          right: 0;
+          border-right: none;
+          right: 0;
+          border-left: 24px solid transparent;
+        }
+      }
+
+      .triangle {
+        /* reset */
+        left: initial;
+        border-right: none;
+        /* reverse */
+        right: 0;
+        border-left: 24px solid transparent;
+      }
+    `};
 `;
 
 const StyledUser = styled.div`
-  border: 2px solid black;
-  background: pink;
+  display: flex;
+  margin-top: 0.5rem;
 
   .avatar {
+    flex: 0 0 48px;
     width: 48px;
     height: 48px;
+    border: 2px solid ${({ theme }) => theme.colors.text};
   }
+
+  .meta {
+    padding: 0 0.5em;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    border: 2px solid ${({ theme }) => theme.colors.text};
+    color: ${({ theme }) => theme.colors.text};
+    border-left: none;
+    background: ${({ theme }) => theme.colors.bg};
+
+    h2,
+    h3 {
+      font-family: "Helvetica", sans-serif;
+      font-size: 0.75em;
+      line-height: 1.33333333;
+    }
+  }
+
+  /* Reverse */
+  ${({ reverse, theme }) =>
+    reverse &&
+    css`
+      flex-direction: row-reverse;
+      text-align: right;
+
+      .meta {
+        border: 2px solid ${theme.colors.text};
+        border-right: none;
+      }
+    `};
 `;
 
 export const SampleReview = {
@@ -92,25 +151,34 @@ export const SampleReview = {
   avatar:
     "https://pbs.twimg.com/profile_images/1054434556156162054/1H_7AxP0.jpg",
   reviewText:
-    "He has contributed extreme value while on the Sprinklr Product Design... Because of his work we have saved countless hours of design & dev work.",
+    "He has contributed extreme value while on the Sprinklr Product Design team... Because of his work we have saved countless hours of design & dev work.",
 };
 
-const Review = ({ children, link, avatar, name, title }) => (
-  <StyledReview>
-    {children}
-    {link && (
-      <a href="#" target="_blank">
-        Read More ↗
-      </a>
-    )}
-    <StyledUser className="user">
+const Review = ({ children, link, avatar, name, title, reverse = false }) => (
+  <StyledReviewGroup>
+    <StyledReview reverse={reverse}>
+      <div className="box">
+        <p>{children}</p>
+        {link && false && (
+          <div>
+            <a href="#" target="_blank">
+              Read More ↗
+            </a>
+          </div>
+        )}
+      </div>
+      {/* <div className="triangle-row">
+        <span className="triangle" />
+      </div> */}
+    </StyledReview>
+    <StyledUser className="user" reverse={reverse}>
       <img src={avatar} alt="profile" className="avatar" />
       <div className="meta">
         <h2>{name}</h2>
         <h3>{title}</h3>
       </div>
     </StyledUser>
-  </StyledReview>
+  </StyledReviewGroup>
 );
 
 export default Review;
