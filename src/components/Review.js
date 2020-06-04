a295f303479bc6830be7ef2365c60f554587e881
Nathan Rothenberger <nroth10@gmail.com>
2020-06-04 15:50:22 -0700

    style reorg

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,17 +3,17 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/core";
 import User from "./User";
 
-const StyledReviewGroup = styled.article`
+const StyledReview = styled.article`
+  background: purple;
   & + & {
-    margin-top: 1.5rem;
     margin-top: 2.5rem;
   }
 `;
 
-const StyledReview = styled.article`
-  background: rgba(0, 200, 0, 0.3);
+const StyledReviewBox = styled.div`
+  background: ${({ theme }) => theme.colors.text};
   position: relative;
-  margin-left: 1rem;
+  padding: 1em;
   margin: 0 0 32px 16px;
 
   p {
@@ -24,29 +24,27 @@ const StyledReview = styled.article`
   }
 
   .box {
-    background: ${({ theme }) => theme.colors.text};
-    position: relative;
-    padding: 1em;
+  }
+  /* triangle */
+  &::after {
+    content: "";
+    position: absolute;
+    display: inline-block;
+    left: 0;
+    top: 100%;
+    border-top: 24px solid ${({ theme }) => theme.colors.text};
+    border-color: hotpink;
+    border-right: 24px solid transparent;
+  }
 
-    &::after {
-      content: "";
-      position: absolute;
-      display: inline-block;
-      left: 0;
-      top: 100%;
-      border-top: 24px solid ${({ theme }) => theme.colors.text};
-      border-right: 24px solid transparent;
-    }
+  /* medium */
+  @media screen and (min-width: 768px) {
+    background-color: red;
+  }
 
-    /* medium */
-    @media screen and (min-width: 768px) {
-      background-color: red;
-    }
-
-    /* large */
-    @media screen and (min-width: 1440px) {
-      background-color: blue;
-    }
+  /* large */
+  @media screen and (min-width: 1440px) {
+    background-color: blue;
   }
 
   /* Reverse */
@@ -56,30 +54,28 @@ const StyledReview = styled.article`
       margin-right: 1em;
       margin-left: 0;
 
+      &::after {
+        content: "";
+        position: absolute;
+        display: inline-block;
+        left: initial;
+        right: 0;
+        border-right: none;
+        right: 0;
+        border-left: 24px solid transparent;
+      }
       .box {
-        &::after {
-          content: "";
-          position: absolute;
-          display: inline-block;
-          left: initial;
-          right: 0;
-          border-right: none;
-          right: 0;
-          border-left: 24px solid transparent;
-        }
       }
     `};
 `;
 
 const Review = ({ children, link, avatar, name, title, reverse = false }) => (
-  <StyledReviewGroup>
-    <StyledReview reverse={reverse}>
-      <div className="box">
-        <p>{children}</p>
-      </div>
-    </StyledReview>
+  <StyledReview>
+    <StyledReviewBox reverse={reverse}>
+      <p>{children}</p>
+    </StyledReviewBox>
     <User avatar={avatar} name={name} title={title} reverse={reverse} />
-  </StyledReviewGroup>
+  </StyledReview>
 );
 
 export default Review;
