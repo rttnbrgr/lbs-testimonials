396e9b8d89db665e8a0e5b4a7edc933c3a89d2fc
Nathan Rothenberger <nroth10@gmail.com>
2020-06-03 17:02:22 -0700

    clean theme

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,16 @@
 export const themeDark = {
   colors: {
     appBg: "#000",
-    primary: "#0a8697",
-    text: "white",
+    text: "#fff",
     bg: "#000",
-    asset: "#ffdc00",
   },
 };
 
 export const themeLight = {
   colors: {
-    appBg: "#ffffff",
-    primary: "#ffdc00",
+    appBg: "#fff",
     text: "#000",
     bg: "#fff",
-    asset: "#0a8697",
   },
 };
 
